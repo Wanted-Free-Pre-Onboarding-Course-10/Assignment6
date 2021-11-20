95aeed46d19b7beb4666483c9a4e2b347181d0f8
taehee <wonderfulhuman@naver.com>
2021-11-21 01:45:23 +0900

    Feat: Authentication을 적용하는 Guards 추가

--- src/charge/charge.controller.ts
+++ src/charge/charge.controller.ts
@@ -1,9 +1,11 @@
-import { Body, Controller, Logger, Post } from '@nestjs/common';
+import { Body, Controller, Logger, Post, UseGuards } from '@nestjs/common';
 import { CreateChargeDto } from './dto/create.charge.dto';
 import { ChargeService } from './charge.service';
 import { GetUser } from '../common/get-user.decorator';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('charge')
+@UseGuards(AuthGuard('jwt'))
 export class ChargeController {
   constructor(private chargeService: ChargeService) {}
 
