c5fe354aeae0bb15424f307bb34a861198d02907
taehee <wonderfulhuman@naver.com>
2021-11-20 11:16:50 +0900

    Feat: 회원가입 로직을 UserController에 추가

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { SignUpDto } from './dto/sign.up.dto';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private userService: UserService) {}
+
+  @Post('sign-up')
+  signUp(@Body() signUpDto: SignUpDto) {
+    console.log(signUpDto);
+    return this.userService.signUp(signUpDto);
+  }
+}
