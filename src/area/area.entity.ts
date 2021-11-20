cd349f56ab219d3bf98c4d4f4716dd5e80c75d99
taehee <wonderfulhuman@naver.com>
2021-11-20 18:32:51 +0900

    Feat: area_boundary, area_center, area_coords를 각각 polygon, point, multipoint 타입에 peScript 타입을 변경했습니다.

--- src/area/area.entity.ts
+++ src/area/area.entity.ts
@@ -1,4 +1,4 @@
-import { Column, Entity, PrimaryGeneratedColumn, Table } from 'typeorm';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity('AREA')
 export class Area {
@@ -6,12 +6,12 @@ export class Area {
   id: number;
 
   @Column({ name: 'area_boundary', type: 'polygon', nullable: false })
-  areaBoundary: string;
+  areaBoundary: number[][];
 
   @Column({ name: 'area_center', type: 'point', nullable: false })
-  areaCenter: string;
+  areaCenter: number[];
   @Column({ name: 'area_coords', type: 'multipoint', nullable: false })
-  areaCoords: string;
+  areaCoords: number[][];
 
   @Column({ name: 'basic_fee', unsigned: true, nullable: false })
   basicFee: number;
