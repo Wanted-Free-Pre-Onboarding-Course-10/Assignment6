d4eb03e2a0a21bdc04876d7f238bcf5121f5f2a0
taehee <wonderfulhuman@naver.com>
2021-11-20 14:13:33 +0900

    Feat: ForbiddenFineRuleServiceImpl, ParkingZoneDiscountServiceImpl 동적으로 Provider로서 주입을 하기위한 Import와 AreaRepository 주입

--- src/charge/charge.module.ts
+++ src/charge/charge.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { ChargeController } from './charge.controller';
 import { ChargeService } from './charge.service';
+import { AreaService } from '../area/area.service';
+import { ParkingZoneDiscountServiceImpl } from '../discount-rule/serviceImpl/ParkingZoneDiscountServiceImpl';
+import { ForbiddenFineRuleServiceImpl } from '../fine-rule/serviceImpl/ForbiddenFineRuleServiceImpl';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AreaRepository } from '../area/area.repository';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([AreaRepository])],
   controllers: [ChargeController],
-  providers: [ChargeService]
+  providers: [
+    ChargeService,
+    AreaService,
+    { provide: 'FineRuleService', useClass: ForbiddenFineRuleServiceImpl },
+    {
+      provide: 'DiscountRuleService',
+      useClass: ParkingZoneDiscountServiceImpl,
+    },
+    ForbiddenFineRuleServiceImpl,
+  ],
 })
 export class ChargeModule {}
