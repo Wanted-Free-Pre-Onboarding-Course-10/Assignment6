37bc3ad45649bdfb6569824e02d88014b4db02fd
taehee <wonderfulhuman@naver.com>
2021-11-20 14:17:08 +0900

    Feat: 로그인을 위한 UserService에서의 로직 추가

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,11 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { SignUpDto } from './dto/sign.up.dto';
 import { UsersRepository } from './user.repository';
+import { SignInDto } from './dto/sign.in.dto';
+import { User } from './user.entity';
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class UserService {
-  constructor(private usersRepository: UsersRepository) {}
+  constructor(
+    private usersRepository: UsersRepository,
+    private jwtService: JwtService,
+  ) {}
   signUp(signUpDto: SignUpDto) {
     return this.usersRepository.signUp(signUpDto);
+  }
+
+  async signIn(signInDto: SignInDto) {
+    const { username, password } = signInDto;
+    const user: User = await this.usersRepository.signIn(username);
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      const payload = { username };
+      const accessToken = this.jwtService.sign(payload);
+
+      return { accessToken };
+    } else throw new UnauthorizedException('logIn failed');
   }
 }
