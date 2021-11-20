ed676428ccca594cb6cb53631b9c4e5d85d03349
taehee <wonderfulhuman@naver.com>
2021-11-20 14:15:53 +0900

    Feat: 로그인을 구현하기 위한 POST user/sign-in 라우트 핸들러 생성

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { SignUpDto } from './dto/sign.up.dto';
 import { UserService } from './user.service';
+import { SignInDto } from './dto/sign.in.dto';
 
 @Controller('user')
 export class UserController {
@@ -8,7 +9,11 @@ export class UserController {
 
   @Post('sign-up')
   signUp(@Body() signUpDto: SignUpDto) {
-    console.log(signUpDto);
     return this.userService.signUp(signUpDto);
+  }
+
+  @Post('sign-in')
+  signIn(@Body() signInDto: SignInDto) {
+    return this.userService.signIn(signInDto);
   }
 }
